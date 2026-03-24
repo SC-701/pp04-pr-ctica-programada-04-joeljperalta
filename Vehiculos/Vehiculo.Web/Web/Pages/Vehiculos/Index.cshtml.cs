9228d5fb6810e7c843a302b796b1427068df00f6
joeljperalta <joel.jppz@gmail.com>
2026-03-24 16:14:53 -0600

    Web Con Seguridad

---
+++ Vehiculos/Vehiculo.Web/Web/Pages/Vehiculos/Index.cshtml.cs
@@ -0,0 +1,56 @@
+using Abstracciones.Interfaces.Reglas;
+using Abstracciones.Modelos;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Infrastructure;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using System.Net;
+using System.Text.Json;
+
+namespace Web.Pages.Vehiculos
+{
+    [Authorize]
+    public class IndexModel : PageModel
+    {
+        private readonly IConfiguracion _configuracion;
+
+        public IList<VehiculoResponse> vehiculos { get; set; } = default!;
+
+        public IndexModel(IConfiguracion configuracion)
+        {
+            _configuracion = configuracion;
+        }
+
+        public async Task OnGet()
+        {
+            string endpoint = _configuracion.ObtenerMetodo("ApiEndPoints", "ObtenerVehiculos");
+            using var cliente = ObtenerClienteConToken();
+
+            var solicitud = new HttpRequestMessage(HttpMethod.Get, endpoint);
+
+            var respuesta = await cliente.SendAsync(solicitud);
+
+            respuesta.EnsureSuccessStatusCode();
+            if (respuesta.StatusCode == HttpStatusCode.OK)
+            {
+                var resultado = await respuesta.Content.ReadAsStringAsync();
+                var opciones = new JsonSerializerOptions { PropertyNameCaseInsensitive = true };
+                vehiculos = JsonSerializer.Deserialize<List<VehiculoResponse>>(resultado, opciones);
+            }
+        }
+
+        private HttpClient ObtenerClienteConToken()
+        {
+            var tokenClaim = HttpContext.User.Claims
+                .FirstOrDefault(c => c.Type == "Token");
+            var cliente = new HttpClient();
+            if (tokenClaim != null)
+                cliente.DefaultRequestHeaders.Authorization =
+                    new System.Net.Http.Headers.AuthenticationHeaderValue(
+                        "Bearer", tokenClaim.Value);
+            return cliente;
+        }
+    }
+
+    }
+
